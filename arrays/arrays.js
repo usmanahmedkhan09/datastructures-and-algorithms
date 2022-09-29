32e20a3246a1e1ff40c95983331094112bdcd8cd
usman ahmed khan <usmanahmedkhan@live.com>
2022-09-29 12:54:49 +0500

    add more operations in custom array class

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -2,39 +2,80 @@ class customArray
 {
     constructor()
     {
-        this.a = {};
+        this.arr = {};
         this.index = 0;
     }
 
-    insert(element)
+    push(element)
     {
-        this.a[this.index] = element;
+        // Time Complexity of this opertaion takes O(1)
+
+        this.arr[this.index] = element;
         this.index++;
     }
 
-    getLength()
+    pop()
+    {
+        // Time Complexity of this opertaion takes O(1)
+
+        let element = this.arr[this.index - 1];
+        this.index--;
+        return element;
+    }
+
+    shift()
+    {
+        // Time Complexity of this opertaion takes O(N) beacuse in shift we remove element from the start and move the next element to the lower index so the arry size reduce 
+
+        for (let i = 0; i < this.size(); i++)
+        {
+            this.arr[i] = this.arr[i + 1];
+        }
+        this.index--;
+    }
+
+    unshift(element)
+    {
+        // Time Complexity of this opertaion takes O(N)
+
+        this.index++;
+        for (let i = this.size(); i >= 0; i--)
+        {
+            this.arr[i] = this.arr[i - 1]
+        }
+        this.arr[0] = element
+        return element;
+    }
+
+    size()
     {
         return this.index;
     }
 
-    printdata()
+    traverse()
     {
+        // Time Complexity of this opertaion takes O(N)
+
         for (let i = 0; i < this.index; i++)
         {
-            console.log(this.a[i])
+            console.log(this.arr[i])
         }
     }
 
     getElementById(index)
     {
-        return this.a[index];
+        return this.arr[index];
     }
 
 }
 
-let arrayOne = new customArray();
+let b = new customArray();
 
-arrayOne.insert('usman');
-arrayOne.insert('ahmed');
-arrayOne.insert('khan');
-console.log(arrayOne.getElementById(2))
+b.push('usman');
+b.push('ahmed');
+b.push('khan');
+// b.shift();
+// b.traverse();
+// console.log(b.size());
+b.unshift('hoorain');
+b.traverse()
