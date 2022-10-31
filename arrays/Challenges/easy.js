0f021a580748b9d2a010dcaca277ee8fa13310a1
usman ahmed khan <usmanahmedkhan@live.com>
2022-10-31 15:18:12 +0500

    new question is added

--- arrays/Challenges/easy.js
+++ arrays/Challenges/easy.js
@@ -111,3 +111,40 @@ function removeElement(nums, val)
 };
 
 removeElement([3, 2, 2, 3], 3);
+
+
+/* 
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+*/
+
+function searchInsertPos(nums, target)
+{
+    for (let i = 0; i < nums.length; i++)
+    {
+        if (nums[i] < target)
+        {
+            continue
+        }
+        return i;
+    }
+    return nums.length
+};
+
+searchInsertPos([1, 3, 5, 6], 5)
